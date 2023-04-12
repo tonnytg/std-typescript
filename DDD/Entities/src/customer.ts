cd9affeb6e8ad5b689bc0cdf9cf3b70a1fb5f927
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-04-11 23:58:52 -0300

    feat: initial commit ddd

---
+++ DDD/Entities/src/customer.ts
@@ -0,0 +1,37 @@
+// This example show Entity
+// If Entity din't have any method with decision of business logic, it is Anemic Entity
+// what is Anemic Entity? https://martinfowler.com/bliki/AnemicDomainModel.html
+
+
+class Customer {
+
+    _id: string;
+    _name: string;
+    _address: string;
+
+    constructor(id: string, name: string, address: string) {
+        this._id = id
+        this._name = name;
+        this._address = address;
+    }
+
+    get id(): string {
+        return this._id;
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
+    get address(): string {
+        return this._address;
+    }
+
+    set name(name: string) {
+        this._name = name;
+    }
+
+    set address(address: string) {
+        this._address = address;
+    }
+}
