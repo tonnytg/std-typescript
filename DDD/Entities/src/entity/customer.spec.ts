d4a570b199a45e8029b488e2a0e1adea368342ec
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-05-01 23:42:34 -0300

    feat: add customer with test in jest

--- DDD/Entities/src/entity/customer.spec.ts
+++ DDD/Entities/src/entity/customer.spec.ts
@@ -1,12 +1,27 @@
+import Order from "./order";
+import OrderItem from './order_item';
+
 describe("Customer unit test", () => {
 
-    it("should throw error when id is empty", () => {
+    it("should be true", () => {
+        expect(true).toBe(true);
+    }
+    );
 
-        expect(() => {
-            let customer = new Customer("", "John");
-        }).toThrowError("Id is required");
+    it ("should return error", () => {
+        expect( () => {
+            let customer = new Customer("1", );
+        }).toThrowError("Name is required");
     });
 
-    it("Should throw error when name is empty")
+    it("should not return error", () => {
+        expect(() => {
+
+            let orderItem1 = new OrderItem("1", "1", "item1", 10);
+            let orderItem2 = new OrderItem("2", "1", "item2", 20);
+            let listOrders = [orderItem1, orderItem2];
+            let order = new Order("1", "1", listOrders, 1);
+        }).toBeTruthy();
+      });
 
 });
--- DDD/Entities/src/entity/customer.ts
+++ DDD/Entities/src/entity/customer.ts
@@ -4,7 +4,7 @@
 
 import { Address } from "./address";
 
-class Customer {
+export default class Customer {
 
     private _id: string;
     private _name: string = "";
@@ -16,13 +16,26 @@ class Customer {
         this._name = name;
         this.validate();
     }
+    get id(): string {
+        return this._id;
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
+    get address(): Address {
+        return this._address;
+    }    
+
+    changeName(name: string) {
+        this._name = name;
+        this.validate();
+    }
 
     validate() {
         if (this._name == "") {
             throw new Error("Name is required")
-        }
-        if (this._id.length != 36) {
-            throw new Error("Invalid id")
         }
     }
 
