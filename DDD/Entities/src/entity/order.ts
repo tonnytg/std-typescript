cf0117e3e3aabf0e3ebac3658c57caf9464a403b
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-05-01 23:42:55 -0300

    feat: ajust orders

--- DDD/Entities/src/entity/order.ts
+++ DDD/Entities/src/entity/order.ts
@@ -1,15 +1,16 @@
 import OrderItem from './order-item';
+
 export default class Order {
     
     _id: string;
     _customerId: string;
-    _itens: OrderItem[];
+    _items: OrderItem[];
     _total: number;
 
-    constructor(id: string, customerId: string, itens: OrderItem[], total: number) {
+    constructor(id: string, customerId: string, _items: OrderItem[], total: number) {
         this._id = id;
         this._customerId = customerId;
-        this._itens = itens;
+        this._items = _items;
         this._total = total;
     }
 
